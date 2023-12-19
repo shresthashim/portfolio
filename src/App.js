e84b4d1049d45d13b5a4334fe60d18742f0f2f55
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-19 19:20:15 +0545

    project added

--- src/App.js
+++ src/App.js
@@ -23,12 +23,11 @@ import TextUImg from "./assets/textutils.png";
 import WeatherImg from "./assets/weather.png";
 import DiceImg from "./assets/dice.png";
 import DictionaryImg from "./assets/dictionary.png";
-import BtrackerImg from "./assets/budget.png"
-
+import BtrackerImg from "./assets/budget.png";
+import chathubImg from "./assets/chathub.png";
 const App = () => {
   const projects = [
     {
-      
       id: 1,
       image: BtrackerImg,
       title: "Budget Tracker",
@@ -36,9 +35,18 @@ const App = () => {
         "A straightforward Expense Management System built with MERN Stack, enabling users to easily track their income and expenses through a simple login interface.",
       liveDemo: "https://income-expense-tracker.cyclic.app/",
       github: "https://github.com/AsHim1123/Budget-Tracker-login-Mern",
-    },   
+    },
     {
       id: 2,
+      image: chathubImg,
+      title: "Budget Tracker",
+      description:
+        "ChatHub, a MERN stack-powered chat app with Socket.IO integration, delivers instant messaging, responsive design, secure authentication. Deployed in Vercel.",
+      liveDemo: "https://yourchathub.vercel.app/",
+      github: "https://github.com/AsHim1123/chat-app",
+    },
+    {
+      id: 3,
       image: tictactoeImg,
       title: "Tic Tac Toe",
       description:
@@ -47,7 +55,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/tic_tac_toe-game",
     },
     {
-      id: 3,
+      id: 4,
       image: budgetcalcImg,
       title: "Budget Calculator",
       description: "An intuitive React-based budget calculator for efficient financial management.",
@@ -55,7 +63,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/Budget-Calculator",
     },
     {
-      id: 4,
+      id: 5,
       image: notesAppImg,
       title: "Notes App",
       description: "A sleek and versatile HTML/CSS/JS note app for organizing thoughts and tasks effortlessly.",
@@ -63,7 +71,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/Note-App",
     },
     {
-      id: 5,
+      id: 6,
       image: TodoImg,
       title: "To-Do List",
       description: "A user-friendly HTML/CSS/JS to-do list app for efficient task management and productivity.",
@@ -71,7 +79,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/To-Do-List-App",
     },
     {
-      id: 6,
+      id: 7,
       image: calcImg,
       title: "Calculator",
       description: "A feature-rich HTML/CSS/JS calculator app for quick and accurate mathematical calculations.",
@@ -79,7 +87,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/Calculator",
     },
     {
-      id: 7,
+      id: 8,
       image: rpsImg,
       title: "Rock Paper Scissors",
       description: "An interactive HTML/CSS/JS rock paper scissors game for fun and competitive decision-making.",
@@ -87,7 +95,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/Rock-Paper-Scissors",
     },
     {
-      id: 8,
+      id: 9,
       image: bmiImg,
       title: "BMI Calculator",
       description: "A user-friendly HTML/CSS/JS BMI calculator app for assessing and tracking body mass index easily.",
@@ -95,7 +103,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/BMI-Calculator",
     },
     {
-      id: 9,
+      id: 10,
       image: PassImg,
       title: "Random Password Generator",
       description:
@@ -104,7 +112,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/Random-Password-Generator1",
     },
     {
-      id: 10,
+      id: 11,
       image: TempImg,
       title: "Temperature Converter",
       description: "A temperature converter made by HTML, CSS and JavaScript",
@@ -112,7 +120,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/Temperature-Converter",
     },
     {
-      id: 11,
+      id: 12,
       image: TodoImg,
       title: "To-Do React App",
       description: "A To-Do React Application made by React and using Bootstrap.",
@@ -120,7 +128,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/To-Do-React-App",
     },
     {
-      id: 12,
+      id: 13,
       image: QRImg,
       title: "QR Code Generator",
       description:
@@ -129,7 +137,7 @@ const App = () => {
       github: " https://github.com/AsHim1123/QR-Code-Generator",
     },
     {
-      id: 13,
+      id: 14,
       image: TextUImg,
       title: "Text Utils React App",
       description: "Customize your texts and paragraphs through this React based Text Utils Application.",
@@ -137,7 +145,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/Text-Utils-React",
     },
     {
-      id: 14,
+      id: 15,
       image: WeatherImg,
       title: "Weather App",
       description: "Stay prepared with real-time weather updates at your fingertips using this weather app.",
@@ -145,7 +153,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/Weather-App",
     },
     {
-      id: 15,
+      id: 16,
       image: DiceImg,
       title: "Dice Roll Simulator",
       description: "Roll the virtual dice and let chance decide your fate.",
@@ -153,7 +161,7 @@ const App = () => {
       github: "https://github.com/AsHim1123/Dice-Roll",
     },
     {
-      id: 16,
+      id: 17,
       image: DictionaryImg,
       title: "Dictionary",
       description: "Find the meaning of words from API based dictionary app. ",
