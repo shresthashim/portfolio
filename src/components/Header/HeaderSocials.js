34fd7725a65ef489ab05bd195525b42d249e6022
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-05 16:19:31 +0545

    refactor : username URL of socials

--- src/components/Header/HeaderSocials.js
+++ src/components/Header/HeaderSocials.js
@@ -4,10 +4,10 @@ import { FaGithub } from "react-icons/fa";
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-      <a href="https://www.linkedin.com/in/ashim-shrestha-a34081275/"  rel="noreferrer" target="_blank">
+      <a href="https://www.linkedin.com/in/shresthashim/"  rel="noreferrer" target="_blank">
         <BsLinkedin className="Link" />
       </a>
-      <a href="https://github.com/AsHim1123"   rel="noreferrer" target="_blank">
+      <a href="https://github.com/shresthashim"   rel="noreferrer" target="_blank">
         <FaGithub className="Link" />
       </a>
     </div>
