767235fdc0b085b67ddfed485c50be89dde17dab
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-13 16:13:30 +0545

    Moviehub project added

--- src/App.js
+++ src/App.js
@@ -27,11 +27,21 @@ import BtrackerImg from "./assets/budget.png";
 import chathubImg from "./assets/chathub.png";
 import ThreeDPortfolioImg from "./assets/ThreeDPortfolio.png";
 import googleImg from "./assets/google.png";
+import moviehubImg from "./assets/moviehub.png";
 
 const App = () => {
   const projects = [
     {
       id: 1,
+      image: moviehubImg,
+      title: "MovieHub",
+      description:
+        "A movie database website showcasing the latest and popular movies. Built with Next Js and Tailwind CSS, this app features a search bar, a watchlist, and a movie detail page.",
+      liveDemo: "https://moviehub-app.vercel.app/",
+      github: "https://github.com/shresthashim/moviehub",
+    },
+    {
+      id: 2,
       image: BtrackerImg,
       title: "Budget Tracker",
       description:
@@ -40,7 +50,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Budget-Tracker-login-Mern",
     },
     {
-      id: 2,
+      id: 3,
       image: ThreeDPortfolioImg,
       title: "My 3d Portfolio",
       description: "A 3D portfolio website built with Vite and Three.js.",
@@ -48,7 +58,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Google-Clone-NextJs14",
     },
     {
-      id: 3,
+      id: 4,
       image: googleImg,
       title: "Google Clone",
       description: "A clone of google made by Next JS 14 using app router and styling by Tailwilnd Css. Deployed in vercel.",
@@ -57,7 +67,7 @@ const App = () => {
     },
 
     {
-      id: 4,
+      id: 5,
       image: chathubImg,
       title: "ChatHub",
       description:
@@ -66,7 +76,7 @@ const App = () => {
       github: "https://github.com/shresthashim/chat-app",
     },
     {
-      id: 5,
+      id: 6,
       image: tictactoeImg,
       title: "Tic Tac Toe",
       description:
@@ -75,7 +85,7 @@ const App = () => {
       github: "https://github.com/shresthashim/tic_tac_toe-game",
     },
     {
-      id: 6,
+      id: 7,
       image: budgetcalcImg,
       title: "Budget Calculator",
       description: "An intuitive React-based budget calculator for efficient financial management.",
@@ -83,7 +93,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Budget-Calculator",
     },
     {
-      id: 7,
+      id: 8,
       image: notesAppImg,
       title: "Notes App",
       description: "A sleek and versatile HTML/CSS/JS note app for organizing thoughts and tasks effortlessly.",
@@ -91,7 +101,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Note-App",
     },
     {
-      id: 8,
+      id: 9,
       image: TodoImg,
       title: "To-Do List",
       description: "A user-friendly HTML/CSS/JS to-do list app for efficient task management and productivity.",
@@ -99,7 +109,7 @@ const App = () => {
       github: "https://github.com/shresthashim/To-Do-List-App",
     },
     {
-      id: 9,
+      id: 10,
       image: calcImg,
       title: "Calculator",
       description: "A feature-rich HTML/CSS/JS calculator app for quick and accurate mathematical calculations.",
@@ -107,7 +117,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Calculator",
     },
     {
-      id: 10,
+      id: 11,
       image: rpsImg,
       title: "Rock Paper Scissors",
       description: "An interactive HTML/CSS/JS rock paper scissors game for fun and competitive decision-making.",
@@ -115,7 +125,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Rock-Paper-Scissors",
     },
     {
-      id: 11,
+      id: 12,
       image: bmiImg,
       title: "BMI Calculator",
       description: "A user-friendly HTML/CSS/JS BMI calculator app for assessing and tracking body mass index easily.",
@@ -123,7 +133,7 @@ const App = () => {
       github: "https://github.com/shresthashim/BMI-Calculator",
     },
     {
-      id: 12,
+      id: 13,
       image: PassImg,
       title: "Random Password Generator",
       description:
@@ -132,7 +142,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Random-Password-Generator1",
     },
     {
-      id: 13,
+      id: 14,
       image: TempImg,
       title: "Temperature Converter",
       description: "A temperature converter made by HTML, CSS and JavaScript",
@@ -140,7 +150,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Temperature-Converter",
     },
     {
-      id: 14,
+      id: 15,
       image: TodoImg,
       title: "To-Do React App",
       description: "A To-Do React Application made by React and using Bootstrap.",
@@ -148,7 +158,7 @@ const App = () => {
       github: "https://github.com/shresthashim/To-Do-React-App",
     },
     {
-      id: 15,
+      id: 16,
       image: QRImg,
       title: "QR Code Generator",
       description:
@@ -157,7 +167,7 @@ const App = () => {
       github: " https://github.com/shresthashim/QR-Code-Generator",
     },
     {
-      id: 16,
+      id: 17,
       image: TextUImg,
       title: "Text Utils React App",
       description: "Customize your texts and paragraphs through this React based Text Utils Application.",
@@ -165,7 +175,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Text-Utils-React",
     },
     {
-      id: 17,
+      id: 18,
       image: WeatherImg,
       title: "Weather App",
       description: "Stay prepared with real-time weather updates at your fingertips using this weather app.",
@@ -173,7 +183,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Weather-App",
     },
     {
-      id: 18,
+      id: 19,
       image: DiceImg,
       title: "Dice Roll Simulator",
       description: "Roll the virtual dice and let chance decide your fate.",
@@ -181,7 +191,7 @@ const App = () => {
       github: "https://github.com/shresthashim/Dice-Roll",
     },
     {
-      id: 19,
+      id: 20,
       image: DictionaryImg,
       title: "Dictionary",
       description: "Find the meaning of words from API based dictionary app. ",
